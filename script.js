6bd9cb8a5591293544ca3a9ee6434437309eb2f5
Garik9402 <101651468+Garik9402@users.noreply.github.com>
2022-11-08 20:41:15 +0300

    in the process;)

---
+++ script.js
@@ -0,0 +1,111 @@
+// // // const objectItems = [{
+// // //    title: 'привет',
+// // //    price: 28,
+// // //    color: 'blue',
+// // // },
+// // // {
+// // //    title: 'рад',
+// // //    price: 40,
+// // //    color: 'red',
+// // // },
+// // // {
+// // //    title: 'шалом',
+// // //    price: 60,
+// // //    color: 'yellow',
+// // // },
+// // // ];
+
+// // // function funcObj(s) {
+// // //    let titleNode = document.querySelector('.header__titles')
+// // //    let priceNode = document.querySelector('.header__price')
+// // //    let colorNode = document.querySelector('.header__colors')
+// // //    s.forEach(el => {
+// // //       let pt = document.createElement('p')
+// // //       let p = document.createElement('p')
+// // //       let div = document.createElement('div')
+// // //       pt.classList.add('title')
+// // //       p.classList.add('price')
+// // //       div.classList.add('color')
+// // //       div.style.background = el.color
+// // //       div.addEventListener('click', function () {
+// // //          titleNode.innerHTML = el.title
+// // //          priceNode.innerHTML = el.price
+// // //       })
+// // //       titleNode.append(pt)
+// // //       priceNode.append(p)
+// // //       colorNode.append(div)
+// // //    });
+// // // }
+// // // funcObj(objectItems)
+// // // async function asincF(w) {
+// // //    let fuc = await (w + 4)
+// // //    console.log(fuc)
+// // // }
+// // // asincF(6)
+
+
+// // const inputsNode = document.querySelectorAll('.header__input')
+// // const buttonNode = document.querySelector('.header__button')
+// // const inputNode = document.getElementById('result')
+// // function funcRes() {
+// //    if (inputNode.value !== 0) {
+// //       let str = inputNode.value;
+// //       let arr = str.split(' ');
+// //       for (let i = 0; i < arr.length; i++) {
+// //          // inputsNode[f].value = arr[i]
+// //          console.log(arr[i])
+// //       }
+// //    }
+// //    // if (inputNode.value == 0) {
+// //    //    inputNode.placeholder = 'введите ваши данные'
+// //    //    for (var f = 0; f < inputsNode.length; f++) {
+// //    //       inputsNode[f].value = ''
+// //    //    }
+// //    // }
+
+// // }
+
+// // inputNode.addEventListener('blur', funcRes)
+// const obj = [{
+//    name: 'Гарик',
+//    fam: 'Бесаев',
+//    otch: 'Сосланович',
+//    age: '28 лет',
+// },
+// {
+//    name: 'Игорь',
+//    fam: 'Меняев',
+//    otch: 'Игоревич',
+//    age: '18 лет',
+// },
+// {
+//    name: 'Артур',
+//    fam: 'Хитяев',
+//    otch: 'Виняевич',
+//    age: '43 лет',
+// }
+// ]
+let paragrafNOde = document.getElementById('para')
+let colorsStr = ['один', 'два', 'три']
+let linkNode = document.querySelector('.link')
+let i = 0;
+
+linkNode.addEventListener('click', start)
+paragrafNOde.innerHTML = colorsStr[i];
+i = 1
+function start() {
+   paragrafNOde.innerHTML = colorsStr[i];
+   i++;
+   if (i >= colorsStr.length) {
+      i = 0
+   }
+}
+
+
+
+
+
+
+
+
+
